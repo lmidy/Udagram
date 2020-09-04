03eb3ef31908035409a2ca475ceef8c2c1b1474b
lmidy <lynette.midy@gmail.com>
2020-09-04 10:45:29 -0500

    Implement image restful endpoint
    Added valid url npm package, addressed todo, tested

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import { isUri } from 'valid-url';
 
 (async () => {
 
@@ -13,23 +14,19 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
   //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
   // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
-
-  /**************************************************************************** */
-
-  //! END @TODO1
+  //validate image_url, call filterimage from utils, send file then delete from server
+  app.get("/filteredimage", async (req: Request, res: Response) => {
+    const { image_url: imageURL } = req.query;
+    if (!imageURL || !isUri(imageURL)) {
+      return res.status(400).send({ auth: false, message: 'Oops Image URL is missing or not valid URL' });
+    }
+    const filteredPath = await filterImageFromURL(imageURL);
+    
+    res.sendFile(filteredPath, {}, () => deleteLocalFiles([filteredPath]));
+  });
+  
   
   // Root Endpoint
   // Displays a simple message to the user
